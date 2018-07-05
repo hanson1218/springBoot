bec5c059959dd81c4fe5b1836873c2c059943889
scli <v-scli@expedia.com>
2018-07-05 09:55:25 +0800

    add rabbitMQ finished
      update
    
     add rabbitMQ

---
+++ src/main/java/com/lhs/www/rabbitmq/topic/TopicProducer.java
@@ -0,0 +1,23 @@
+package com.lhs.www.rabbitmq.topic;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.lhs.www.common.Constant;
+
+
+@Component
+public class TopicProducer {
+
+	@Autowired
+	private AmqpTemplate rabbitTemplate;
+	
+	public void send() {
+        String msg1 = "I am topic.mesaage msg======";
+        this.rabbitTemplate.convertAndSend(Constant.TOPIC_EXCHANGE, Constant.TOPIC_MESSAGE, msg1);
+        
+        /*String msg2 = "I am topic.mesaages msg########";
+        this.rabbitTemplate.convertAndSend(Constant.TOPIC_EXCHANGE, Constant.TOPIC_MESSAGES, msg2);*/
+    }
+}
